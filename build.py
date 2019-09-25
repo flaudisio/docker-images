1cb7bd1596665a7c629e7f50fc9056692a6ccc1c
Flaudísio Tolentino <flaudisio@gmail.com>
2019-09-25 00:38:00 -0300

    Add build script

---
+++ build.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+import subprocess
+import sys
+import yaml
+
+
+DEBUG = os.getenv("DEBUG")
+DRY_RUN = os.getenv("DRY_RUN")
+
+SPEC_FILE = "buildspec.yml"
+DOCKERFILE = "Dockerfile"
+
+DOCKER_REPO = os.getenv("DOCKER_REPO", "docker.io/flaudisio")
+
+IMAGE = os.getenv("IMAGE", "all")
+IMAGES_DIR = "images"
+
+
+class cd:
+    """
+    Context manager for changing the current working directory.
+    """
+    def __init__(self, new_path):
+        self.new_path = os.path.expanduser(new_path)
+
+    def __enter__(self):
+        self.saved_path = os.getcwd()
+        os.chdir(self.new_path)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.saved_path)
+
+
+def show_debug(message: str) -> None:
+    if DEBUG:
+        print(f"[DEBG] {message}")
+
+
+def show_info(message: str) -> None:
+    print(f"[INFO] {message}")
+
+
+def show_warn(message: str) -> None:
+    print(f"[WARN] {message}", file=sys.stderr)
+
+
+def show_error(message: str, **kwargs) -> None:
+    print(f"[ERROR] {message}", file=sys.stderr)
+
+    if kwargs.get("exit"):
+        sys.exit(1)
+
+
+def get_image_dirs(image_name: str) -> list:
+    """
+    Return all image directories.
+    """
+    glob_path = "*" if image_name == "all" else image_name
+
+    image_dirs = glob.glob(os.path.join(os.getcwd(), IMAGES_DIR, glob_path))
+
+    return sorted(image_dirs)
+
+
+def build_image(image: dict, build_dir: str) -> None:
+    image_name = image["name"]
+
+    for image_tag in image.get("tags"):
+        tag_name = image_tag["name"]
+        tag_aliases = image_tag.get("aliases", [])
+        build_args = image_tag.get("build_args", [])
+        dockerfile = image_tag.get("dockerfile", DOCKERFILE)
+
+        image_repo = f"{DOCKER_REPO}/{image_name}"
+        image_fullname = f"{image_repo}:{tag_name}"
+
+        docker_build_cmd = [
+            "docker", "image", "build", "--rm", "--force-rm",
+            f"--file={dockerfile}",
+            f"--tag={image_fullname}",
+            "."
+        ]
+
+        for build_arg in build_args:
+            docker_build_cmd.append(f"--build-arg={build_arg}")
+
+        show_info(f"Entering directory {build_dir}")
+
+        with cd(build_dir):
+            show_info(f"Building: " + " ".join(docker_build_cmd))
+            subprocess.run(docker_build_cmd)
+
+            for alias in tag_aliases:
+                docker_tag_cmd = [
+                    "docker", "image", "tag",
+                    image_fullname,
+                    f"{image_repo}:{alias}"
+                ]
+
+                show_info(f"Tagging: " + " ".join(docker_tag_cmd))
+                subprocess.run(docker_tag_cmd)
+
+
+
+def main():
+    image_dirs = get_image_dirs(image_name=IMAGE)
+
+    if len(image_dirs) == 0:
+        show_info(f"Nothing found in '{IMAGES_DIR}/'; exiting")
+        sys.exit(0)
+
+    for image_dir in image_dirs:
+        spec_filepath = os.path.join(image_dir, SPEC_FILE)
+
+        if not os.path.isdir(image_dir):
+            show_debug(f"Not a dir: {image_dir}; ignoring")
+            continue
+
+        if not os.path.exists(spec_filepath):
+            show_info(f"File {SPEC_FILE} not found in {image_dir}; ignoring")
+            continue
+
+        with open(spec_filepath, "r") as spec_file:
+            show_debug(f"Loading file {spec_filepath}")
+
+            try:
+                buildspec = yaml.safe_load(spec_file)
+            except yaml.YAMLError as exc:
+                show_error(exc, exit=1)
+
+            show_debug("Buildspec: {}".format(buildspec))
+
+            for image in buildspec.get("images"):
+                show_info("Processing image: " + image["name"])
+
+                if DRY_RUN:
+                    show_info("Dry run mode enabled; not building")
+                    continue
+
+                build_image(image=image, build_dir=image_dir)
+
+
+if __name__ == '__main__':
+    main()
