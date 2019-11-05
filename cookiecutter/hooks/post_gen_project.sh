4b342e683baf94f35f3b5dd48e78ff2d7ac5f805
Flaudísio Tolentino <flaudisio@gmail.com>
2019-11-05 01:32:19 -0300

    Cookiecutter: add option for selecting the image type

---
+++ cookiecutter/hooks/post_gen_project.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+ImageType="{{ cookiecutter.image_type }}"
+
+ImageSourceDir="{{ cookiecutter.image_slug }}"
+ImageDestDir="./base-images"
+
+if echo "$ImageType" | grep -q 'Child Image' ; then
+    ImageDestDir="./child-images"
+fi
+
+cd ..
+
+if [ -d "$ImageDestDir" ] ; then
+    echo "Moving '$ImageSourceDir' to '$ImageDestDir'"
+    mv -i "$ImageSourceDir" "$ImageDestDir"
+else
+    echo "Warning! Directory '$ImageDestDir' not found; keeping '$ImageSourceDir' in current directory."
+fi
