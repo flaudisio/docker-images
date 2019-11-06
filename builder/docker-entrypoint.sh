e821834fa792591de11c35d3af2118afd65ce027
Flaudísio Tolentino <flaudisio@gmail.com>
2019-11-06 00:07:20 -0300

    Builder: add support for ENABLE_PUSH

---
+++ builder/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+if [ "$ENABLE_PUSH" = "true" ] ; then
+    echo "==> Logging on Docker Hub"
+    echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+    echo
+fi
+
+case $1 in
+    build)
+        shift
+        echo "==> Running build script"
+        echo
+        exec pydib "$@"
+    ;;
+esac
+
+exec "$@"
