1e0992c366e6e4d258f8909ffa01879442e98037
Flaudísio Tolentino <flaudisio@gmail.com>
2026-05-26 17:17:17 -0300

    refactor(semaphore-runner): use common entrypoint and base image

--- semaphore-runner/entrypoint.sh
+++ semaphore-runner/entrypoint.sh
@@ -33,24 +33,21 @@ function setup_ssh_client()
 function setup_semaphore_files()
 {
     local -r error=0
-    local -r required_dirs=(
+    local -r common_dirs=(
         "$CONFIG_DIR"
         "$SEMAPHORE_TMP_PATH"
     )
-    local -r required_files=(
-        "$SEMAPHORE_RUNNER_TOKEN_FILE"
-        "$SEMAPHORE_RUNNER_PRIVATE_KEY_FILE"
-    )
+    local -r required_files=( "$@" )
     local path
 
-    for path in "${required_dirs[@]}" ; do
+    for path in "${common_dirs[@]}" ; do
         if [[ ! -d "$path" ]] ; then
             _msg "Creating $path"
             mkdir -p "$path"
         fi
     done
 
-    for path in "${required_dirs[@]}" "${required_files[@]}" ; do
+    for path in "${common_dirs[@]}" "${required_files[@]}" ; do
         if ! gosu semaphore touch "$path" 2> /dev/null ; then
             _msg "Fixing $path permissions"
 
@@ -64,10 +61,39 @@ function setup_semaphore_files()
     [[ $error -eq 0 ]] || exit 1
 }
 
+function setup_admin_user()
+{
+    if [[ -z "$SEMAPHORE_ADMIN" ]] ; then
+        _msg "Skipping admin user setup because variable SEMAPHORE_ADMIN is not defined"
+        return
+    fi
+
+    _msg "Configuring admin user"
+
+    local user_cmd="add"
+
+    if semaphore --no-config user list | grep -q "^${SEMAPHORE_ADMIN}\$" ; then
+        user_cmd="change-by-login"
+    fi
+
+    semaphore user "$user_cmd" \
+        --no-config \
+        --admin \
+        --login "$SEMAPHORE_ADMIN" \
+        --name "$SEMAPHORE_ADMIN_NAME" \
+        --email "$SEMAPHORE_ADMIN_EMAIL" \
+        --password "$SEMAPHORE_ADMIN_PASSWORD"
+}
+
 function register_runner()
 {
     if [[ -n "$SEMAPHORE_RUNNER_TOKEN" ]] ; then
         _msg "Variable SEMAPHORE_RUNNER_TOKEN is defined, skipping runner registration"
+        return 0
+    fi
+
+    if [[ -s "$SEMAPHORE_RUNNER_TOKEN_FILE" && -s "$SEMAPHORE_RUNNER_PRIVATE_KEY_FILE" ]] ; then
+        _msg "Found runner token and private key files, skipping runner registration"
         return 0
     fi
 
@@ -76,18 +102,33 @@ function register_runner()
 
         echo -n "$SEMAPHORE_RUNNER_REGISTRATION_TOKEN" \
             | semaphore runner register --stdin-registration-token --no-config
+
+        _msg "Runner successfully registered!"
+        _msg "NOTE: to ENABLE the runner, go to ${SEMAPHORE_WEB_ROOT}/runners"
+        return 0
     fi
+
+    _msg "Warning: no token/registration configuration has been found!"
 }
 
 case "$1" in
     semaphore)
-        if [[ "$2" == "runner" && "$3" == "start" ]] ; then
-            setup_ssh_client
+        if [[ "$2" = "server" ]] ; then
             setup_semaphore_files
+            setup_admin_user
+        fi
+
+        if [[ "$2" == "runner" && "$3" == "start" ]] ; then
+            setup_semaphore_files \
+                "$SEMAPHORE_RUNNER_TOKEN_FILE" \
+                "$SEMAPHORE_RUNNER_PRIVATE_KEY_FILE"
+
+            setup_ssh_client
             register_runner
         fi
 
-        _msg "Running: $*"
+        _msg "Running in $2 mode"
+        _msg "+ $*"
         exec gosu semaphore "$@"
     ;;
 esac
