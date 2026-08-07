2a776d1eaccd7e3c0a766804c5fab4a1786f2c92
Flaudísio Tolentino <flaudisio@gmail.com>
2026-08-07 18:46:14 -0300

    chore(transfer): simplify and improve image

--- transfer.sh/entrypoint.sh
+++ transfer.sh/entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 set -o pipefail
 
 
-msg()
+_msg()
 {
     echo "$( date -Iseconds ) [entrypoint] $*" >&2
 }
@@ -20,16 +20,16 @@ set_files_ownership()
         "$TEMP_PATH"
     do
         if [ ! -d "$data_dir" ] ; then
-            msg "[INFO] Creating $data_dir"
+            _msg "Creating $data_dir"
             mkdir -p "$data_dir"
         fi
 
         # Try to create/modify a file using the transfer.sh user
         if ! gosu "$TRANSFER_USER" touch "${data_dir}/.docker-entrypoint-probe" 2> /dev/null ; then
-            msg "[INFO] Fixing permissions for $data_dir"
+            _msg "Fixing permissions for $data_dir"
 
             if ! chown -R "$TRANSFER_USER" "$data_dir" ; then
-                msg "[ERROR] Could not fix $data_dir permissions; please check your environment. Aborting Docker entrypoint" >&2
+                _msg "Fatal: could not fix $data_dir permissions; please check your environment. Aborting Docker entrypoint" >&2
                 exit 1
             fi
         fi
@@ -40,7 +40,7 @@ case "$1" in
     transfer)
         set_files_ownership
 
-        msg "[INFO] Running transfer.sh"
+        _msg "Running transfer.sh"
         exec gosu "$TRANSFER_USER" "$@"
     ;;
 esac
